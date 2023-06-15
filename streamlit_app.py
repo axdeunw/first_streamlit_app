da707a00b9b0d21b6f4605e21e58b6e29363cbc0
axdeunw <118701477+axdeunw@users.noreply.github.com>
2023-06-15 17:24:17 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Kale, Spinachh & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
